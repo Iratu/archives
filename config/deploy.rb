2315517b937cbc098d2b5f8d2762a84838bae010
Iratu <admin@rori-dl.com>
2018-11-17 15:59:38 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,5 +2,5 @@ set :application, "archives"
 set :repo_url, "git://github.com/Iratu/archives.git"
 set :deploy_to, "/var/www/archives"
 set :rbenv_type, :user
-set :rbenv_ruby, "2.3.1"
+set :rbenv_ruby, "2.3.7"
 set :linked_files, fetch(:linked_files, []).push(".env")
